4b9383ede95f54cf9faf19631370a707fb708942
Anca Simon <ancarsimon@gmail.com>
2020-08-15 11:43:41 -0500

    Add project files.

---
+++ TheHeist/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using TheHeist.Team;
+
+namespace TheHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tryAgain = null;
+            var team = new List<TeamMember>();
+
+            //Phase 1: Get a team member's name and store it!
+            do
+            {
+                Console.WriteLine("Plan Your Heist!");
+                Console.WriteLine("What's the team member's name?");
+                var teamMemberName = Console.ReadLine();
+                if (string.IsNullOrEmpty(teamMemberName))
+                {
+                    Console.WriteLine("You must enter a name!");
+                }
+                else
+                {
+                    Console.WriteLine("What's the team member's skill level?");
+                    var teamMemberSkillLevel = Console.ReadKey().KeyChar;
+                    if (teamMemberSkillLevel == 13)  //Anca : 13 is the key for Enter
+                    {
+                        Console.WriteLine("You must enter a skill level!");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("What's the team member's courage factor?");
+                        var teamMemberCourageFactor = Console.ReadKey().KeyChar;
+                        if (teamMemberCourageFactor == 13)
+                        {
+                            Console.WriteLine("You must enter a courage level!");
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(@$"You have entered the following team member's information:
+Name: {teamMemberName};
+Skill Level: {teamMemberSkillLevel};
+Courage Factor: {teamMemberCourageFactor};
+");
+                            var member1 = new TeamMember()
+                            {
+                                Name = teamMemberName,
+                                SkillLevel = teamMemberSkillLevel.ToString(),
+                                CourageFactor = teamMemberCourageFactor.ToString()
+                            };
+
+                            //Console.WriteLine(member1.ToString());
+                            team.Add(member1);
+                        }
+                    }
+                        
+                    
+
+                }
+                foreach(var name in team)
+                {
+                    Console.WriteLine($"{name.Name} has a skill level of {name.SkillLevel} and a courage factor of {name.CourageFactor}.");
+                }
+
+                Console.WriteLine("Would you like to add a team member? (Enter Yes or leave blank if No!)");
+                tryAgain = Console.ReadLine();
+            }
+            while (tryAgain != "");
+
+        }
+    }
+}
---
+++ TheHeist/Team/TeamMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Text;
+
+namespace TheHeist.Team
+{
+    class TeamMember
+    {
+        //properties
+        public string Name { get; set; }
+        public string SkillLevel { get; set; } //how can I force it to be a positive number? Is byte an ok way to do that? Doesn't seem to do it!! Even if the max then has to be 255?
+        public string CourageFactor { get; set; } //how can I force a range? AND - how can I store it as a decimal from the Console user entry??
+        //[Range(typeof(decimal), "0.0", "2.0")]
+    }
+}
